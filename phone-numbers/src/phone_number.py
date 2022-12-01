54794b4542efe9c65952141303fd7a863854cb66
Daniel <dsalvatori2008@gmail.com>
2022-12-01 11:41:24 +0000

    added return error

--- phone-numbers/src/phone_number.py
+++ phone-numbers/src/phone_number.py
@@ -1,7 +1,9 @@
 
 def create_phone_number(integer):
     number_string = str(integer)
-    part1 = number_string[0:3]
-    part2 = number_string[3:6]
-    part3 = number_string[6:10]
-    return f'({part1}) {part2}-{part3}'
+    if len(number_string) == 10:
+        part1 = number_string[0:3]
+        part2 = number_string[3:6]
+        part3 = number_string[6:10]
+        return f'({part1}) {part2}-{part3}'
+    return False
--- phone-numbers/tests/phone_number_test.py
+++ phone-numbers/tests/phone_number_test.py
@@ -5,3 +5,7 @@ class TestPhoneNumber(unittest.TestCase):
     def test_create_phone_number_works(self):
         phone_number = create_phone_number(1234567890)
         self.assertEqual("(123) 456-7890", phone_number)
+
+    def test_phone_number_doesnt_work(self):
+        phone_number = create_phone_number(123456789)
+        self.assertEqual(False, phone_number)
